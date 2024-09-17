9aaab15777d3cced2e6efdebfb8431ed06166452
skwwnlpro <skwwnlpro@gmail.com>
2024-09-18 02:07:51 +0900

    feat: add signup and email verification

--- src/users/models.py
+++ src/users/models.py
@@ -1,36 +1,7 @@
-from typing import Any, Dict
-
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-
+from users.manager import UserManager
 from common.models import BaseModel
-
-
-class UserManager(BaseUserManager["Users"]):
-    def create_user(self, email: str, password: str, **extra_fields: Dict[str, Any]) -> "Users":
-        if not email:
-            raise ValueError("Email 주소를 필수로 입력하세요.")
-        if password is None:
-            raise ValueError("Password를 필수로 입력하세요.")
-        if not (extra_fields.get("phone_number") and extra_fields.get("nickname")):
-            raise ValueError("Phone번호와 Nickname은 필수로 입력하세요.")
-
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-
-        user.set_password(password)  # hash + salt
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(
-        self, email: str, password: str, **extra_fields: Dict[str, Any]
-    ) -> "Users":
-        if not (extra_fields.get("phone_number") and extra_fields.get("nickname")):
-            raise ValueError("Phone번호와 Nickname은 필수로 입력하세요.")
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_admin", True)
-        user = self.create_user(email, password=password, **extra_fields)
-        user.save(using=self._db)
-        return user
 
 
 class Users(AbstractBaseUser, PermissionsMixin, BaseModel):
@@ -48,7 +19,7 @@ class Users(AbstractBaseUser, PermissionsMixin, BaseModel):
 
     USERNAME_FIELD: str = "email"
     # Email 이외에 사용자 생성에서 필수 필드
-    REQUIRED_FIELDS: list[str] = ["nickname"]
+    REQUIRED_FIELDS: list[str] = ["nickname", "name", "phone_number"]
 
     objects = UserManager()  # 유저를 생성 및 관리 (유저를 구분해서 관리하기 위해 - 관리자계정, 일반계정)
 
---
+++ src/utils/email.py
@@ -0,0 +1,21 @@
+from django.core.mail import EmailMessage
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from rest_framework.reverse import reverse
+from utils.token import user_activation_token
+
+
+def message(domain, uidb64, token):
+    activation_link = reverse("activate", kwargs={"uidb64": uidb64, "token": token})
+    return f"아래 링크를 클릭.\n\n회원가입 링크 : http://{domain}{activation_link}"
+
+
+def send_verification_email(user, domain):
+    uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+    token = user_activation_token.make_token(user)
+    message_data = message(domain, uidb64, token)
+
+    mail_title = "이메일 인증을 완료해주세요"
+    mail_to = user.email
+    email = EmailMessage(mail_title, message_data, to=[mail_to])
+    email.send()
